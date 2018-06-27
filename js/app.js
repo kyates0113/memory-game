3aa6ed6dca387d855ab24c99166e6a68c2f09982
Kristine Yates <kristine.e.yates@gmail.com>
2018-06-26 20:47:24 -0500

    Added star removal functionality

--- js/app.js
+++ js/app.js
@@ -41,6 +41,7 @@ wholeDeck.addEventListener('click', function(event) {
     if (cardList.length === 2) {
       addMove();
       cardsMatch();
+      checkScore();
     };
   }
 });
@@ -79,7 +80,7 @@ function cardsMatch () {
     }
 }
 
-//move counter to count moves and replace text 
+//move counter to count moves and replace text
 let moveCount = 0;
 function addMove(){
   moveCount++;
@@ -87,14 +88,18 @@ function addMove(){
   numMovesText.innerHTML = moveCount;
 };
 
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+//reduce stars as more moves happen
+function checkScore(){
+  if (moveCount === 7 || moveCount === 21 || moveCount === 14) {
+    removeStar();
+  }
+}
+function removeStar() {
+  const stars = document.querySelectorAll('.stars li');
+  for (star of stars) {
+    if(star.style.display !== 'none'){
+      star.style.display = 'none';
+      break;
+    }
+  }
+}
